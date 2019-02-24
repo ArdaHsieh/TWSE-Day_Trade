1afbe3fcb614327a2fc7d7da975c9f56277358f0
Arda Hsieh <arda800411@gmail.com>
2019-02-25 04:18:27 +0800

    fix rsi problem which caused by wantgoo html changing.

--- techanalysis.py
+++ techanalysis.py
@@ -25,8 +25,7 @@ class techmethod:
               + 'StockNo=' + stockNum 
               + '&Kcounts=10&Type=%E6%97%A5K_RSI%7C' )
         
-        rsiDataText = json.loads(bcmethod.htmlgetter().geturl(url))['returnValues']['value']
-        
+        rsiDataText = json.loads(bcmethod.htmlgetter().geturl(url))['returnValues']['value']  
         data = rsiDataText.split('=')
 
         RsiData = eval(data[int(days/5)].split(';')[0])
@@ -45,21 +44,18 @@ class techmethod:
     # Return a float.
     def ma(self, stockNum, date, days=20):
         Days = [5, 10, 20, 60, 120]
-        dateTS = '[' + str(int((bcmethod.timetrans().timestamp(date)))) + '000'
-        url = ( 'https://www.wantgoo.com/stock/'
-              + '%E5%80%8B%E8%82%A1%E7%B7%9A%E5%9C%96/%E6%8A%80%E8%A1%93%E7%B7%9A%E5%9C%96%E8%B3%87%E6%96%99?'
-              + 'StockNo=' + stockNum
-              + '&Kcounts=120&Type=%E6%97%A5K_K%E7%B7%9A%7C%E6%97%A5&isCleanCache=false')
+        dateTS = int((bcmethod.timetrans().timestamp(date)))*1000
+        url = ( 'https://www.wantgoo.com/stock/techchart/highchartdata?'
+              + 'StockNo=' + stockNum 
+              + '&Kcounts=10&Type=日K_K線%7C日' )
         
         maDataText = json.loads(bcmethod.htmlgetter().geturl(url))['returnValues']['value']
         data = maDataText.split('=')
         
         i = Days.index(days) + 3
-        maData = data[i].split(';')
-        MaData = maData[0].split(',')
-        MaData = MaData[::-1]
+        MaData = eval(data[i].split(';')[0])
         
         for j in range(len(MaData)):
-            if MaData[j] == dateTS:
-                return float(MaData[j-1][0:-2])
+            if dateTS == MaData[j]['x']:
+                return MaData[j]['y']
                 break                    
---
+++ test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import techanalysis as ta
+
+print(ta.techmethod().ma('2337','20190222'))
+
